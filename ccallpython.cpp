df9cf32f78cf41bdf95829858a06a81d42f56208
Kent <jkentw2000@gmail.com>
2023-10-10 19:15:18 -0400

    added parameter passing example

--- ccallpython.cpp
+++ ccallpython.cpp
@@ -1,7 +1,37 @@
 // ccallpython.cpp
 #include <Python.h>//If compile error says cant find Python.h try change to #include<Python/Python.h>
+#include <iostream>
+
+// Sample data, i.e. for an image
+const int testWidth = 1000;
+const int testHeight = 1000; 
+int testData[testHeight][testWidth];
+
+// This is the format of the data passed to Python
+struct DataFormat {
+    int *data2D;
+    int width;
+    int height;
+    int stride;	
+} fmt;
+
+// Initialize data to pass to Python
+void initializeTestData() {
+    for(int y = 0; y < testHeight; y++) {
+        for(int x = 0; x < testWidth; x++) {
+            testData[y][x] = testWidth * y + x;
+        }
+    }
+    
+    fmt.data2D = reinterpret_cast<int *>(testData);
+    fmt.width = testWidth;
+    fmt.height = testHeight;
+    fmt.stride = sizeof(testData[0][0]);
+}
 
 int main() {
+    initializeTestData();
+
     // Initialize the Python interpreter
     Py_Initialize();
     // This is important as if you dont import sys and add current path to sys path, C can not locate python module.
@@ -16,13 +46,23 @@ int main() {
 
     // Check if the module was imported successfully
     if (pModule != NULL) {
+    	// Pass a pointer to C memory using this object
+        PyObject *mem = PyMemoryView_FromMemory(reinterpret_cast<char *>(&fmt), sizeof(fmt), PyBUF_READ);
+        
+        // Use this tuple to pass data to the Python module
+        PyObject *dataToPass = PyTuple_New(1);
+        PyTuple_SET_ITEM(dataToPass, 0, mem);
+        
         // Get a reference to the Python function
         PyObject *pFunc = PyObject_GetAttrString(pModule, "hello");
+
+	// Print address:
+	std::cout << "[C++] Array data at address " << fmt.data2D << std::endl;
 
         // Check if the function exists
         if (PyCallable_Check(pFunc)) {
             // Call the Python function
-            PyObject_CallObject(pFunc, NULL);
+            PyObject_CallObject(pFunc, dataToPass);
         } else {
             PyErr_Print();
         }
--- p2ctest.py
+++ p2ctest.py
@@ -1,2 +1,15 @@
-def hello():
+import ctypes
+
+def hello(data):
     print("Hello from python!")
+    
+    if(data is not None):
+    	# 2D array of 4-byte integers
+    	ptr = ctypes.cast(data[0] | data[1] << 8 | data[2] << 16 | data[3] << 24 | data[4] << 32 | data[5] << 40 | data[6] << 48 | data[7] << 56, ctypes.POINTER(ctypes.c_int));
+    	print("[Py ] Address of 2D data is " + str(ptr))
+    	# Prints 10001st element of array
+    	print("[Py ] Test: " + str(ptr[10000]));
+    	# Width of 2D data
+    	print("[Py ] Width: " + str(data[8] | data[9] << 8 | data[10] << 16 | data[11] << 24));
+    	
+    	
