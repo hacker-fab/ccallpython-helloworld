8e4c113860c880e66301e93bd513f745662aa39c
Kent <jkentw2000@gmail.com>
2023-10-21 23:06:12 -0400

    multithreaded Py-C++ with callbacks

--- multithreading-tests/python_master.cpp
+++ multithreading-tests/python_master.cpp
@@ -1,13 +1,103 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "submodule.hpp"
 
+//cross-platform implementation
+#ifdef _WIN32
+#include <Windows.h>
+//TODO: #include windows pthread implementation
+#define SLEEP(x) Sleep(x * 1000)
+#else
+#include <pthread.h>
+#include <unistd.h>
+#define SLEEP(x) sleep(x)
+#endif
+
+//global variables
+
+struct DataFormat {
+    int x;
+    int y;
+    int z;
+} data;
+
 Submodule submodule(10);
+void (*dataCallback)(char *data);
+bool shouldExit;
+
+int pthreadReturn;
+
+//external functions
 
 extern "C" float testFromPy(int val) {
     return 2.5 * submodule.test(val);
 }
 
+extern "C" void setDataCallback(void (*callback)(char *data)) {
+    dataCallback = callback;
+}
+
+extern "C" void exitCpp() {
+    shouldExit = true;
+}
+
+//internal functions
+
+void updateData() {
+    data.x = (int) rand() % 100 - 100;
+    data.y = (int) rand() % 100 - 50;
+    data.z = (int) rand() % 100 - 0;
+
+    if(dataCallback) {
+        dataCallback(reinterpret_cast<char *>(&data));
+    }
+}
+
+void *newRoutine(void *data) {
+    std::cout << "[C++] New thread started successfully." << std::endl;
+
+    while(!shouldExit) {
+        SLEEP(3);
+        updateData();
+    }
+
+    pthreadReturn = 0;
+    shouldExit = false;
+    std::cout << "[C++] Secondary thread exited successfully." << std::endl;
+    pthread_exit(&pthreadReturn);
+}
+
+//entry point; also external
+
 int main(int argc, char *argv[]) {
-    std::cout << "main function called" << std::endl;
+    bool startNewThread = false;
+    pthread_t threadId;
+    void *pthreadReturnPtr = &pthreadReturn;
+
+    std::cout << "[C++] main function called." << std::endl;
+
+    //bad parsing but it works for this example
+    if(argc == 2 && argv[1][0] == '1') { 
+        startNewThread = true;
+        std::cout << "[C++] Starting new thread..." << std::endl;
+    }
+    else {
+        std::cout << "[C++] Not starting new thread." << std::endl;
+    }
+
+    shouldExit = false;
+    dataCallback = nullptr;
+    srand(time(nullptr));
+    data.x = 0;
+    data.y = 0;
+    data.z = 0;
+
+    if(startNewThread) {
+        pthread_create(&threadId, nullptr, newRoutine, nullptr);
+        //pthread_join(threadId, reinterpret_cast<void **>(&pthreadReturnPtr));
+    }
+
+    std::cout << "[C++] Original thread exited successfully." << std::endl;
     return 0;
 }
--- multithreading-tests/python_master.py
+++ multithreading-tests/python_master.py
@@ -1,12 +1,61 @@
 import ctypes
 import pathlib
+import time
 
+# configuration variables
+callbacksLeft = 3
+useNewThread = "1" # example code will hang if set to 0; keep as 1
+
+# utility/test functions
+
+def callback(data_ptr):
+    global callbacksLeft
+
+    if(data_ptr is None):
+        print("[Py ] Error with callback data.")
+        return
+
+    x = ctypes.c_int(int.from_bytes(data_ptr[0:4], byteorder='little')).value # endianness is system-dependent
+    y = ctypes.c_int(int.from_bytes(data_ptr[4:8], byteorder='little')).value
+    z = ctypes.c_int(int.from_bytes(data_ptr[8:12], byteorder='little')).value
+    print(f"[Py ] Received updated data from C++: x={x}, y={y}, z={z}")
+    callbacksLeft -= 1
+
+# main code
+
+print("Started python module. Loading C++ library...")
+
+# load the shared library in the build path 
 lib_path = pathlib.Path().absolute() / "build" / "python_master.so"
 lib = ctypes.CDLL(lib_path)
 
-lib.main()
+# configure library return types
 lib.testFromPy.restype = ctypes.c_float
+lib.main.argtypes = ctypes.c_int, ctypes.POINTER(ctypes.c_char_p)
 
-print("C++ output is " + str(lib.testFromPy(1)))
-print("C++ output is " + str(lib.testFromPy(2)))
-print("C++ output is " + str(lib.testFromPy(3)))
+# call main to initialize library. starts a new thread dedicated for C++ execution
+main_argv = (ctypes.c_char_p * 2)(
+    ctypes.c_char_p("python_master.py".encode('utf-8')), 
+    ctypes.c_char_p(useNewThread.encode('utf-8'))
+)
+lib.main(2, ctypes.cast(main_argv, ctypes.POINTER(ctypes.c_char_p)))
+
+# test a C++ function
+print("[Py ] C++ output is " + str(lib.testFromPy(1)))
+print("[Py ] C++ output is " + str(lib.testFromPy(2)))
+print("[Py ] C++ output is " + str(lib.testFromPy(3)))
+
+# configure data forwarding from C++
+lib.setDataCallback(ctypes.CFUNCTYPE(None, ctypes.c_char_p)(callback))
+
+# let program run for a little
+while(callbacksLeft > 0):
+    time.sleep(1)
+
+# end the C++ thread
+print("[Py ] Calling C++ exit")
+lib.exitCpp()
+
+# wait for C++ threads to end (there should be a better way than this)
+time.sleep(5)
+print("[Py ] End of python code reached")
